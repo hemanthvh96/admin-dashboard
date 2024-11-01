8cc54167fbe2a45988ad9b5b27bcf5ca92145e56
Hemanth Vennelakanti <hvennelakanti@hawk.iit.edu>
2024-11-01 12:52:39 -0500

    vitest globals and jest-dom install and add different methods on screen test method

---
+++ src/pages/login/login.spec.tsx
@@ -0,0 +1,18 @@
+import { describe, expect, it } from "vitest";
+import { render, screen } from "@testing-library/react";
+import Login from "./login";
+
+describe("Login page", () => {
+  it("should render with required fields", () => {
+    render(<Login />);
+
+    expect(screen.getByText("Sign in")).toBeInTheDocument();
+    expect(screen.getByPlaceholderText("Username")).toBeInTheDocument();
+    expect(screen.getByPlaceholderText("Password")).toBeInTheDocument();
+    expect(screen.getByRole("button", { name: "Login" })).toBeInTheDocument();
+    expect(
+      screen.getByRole("checkbox", { name: "Remember me" })
+    ).toBeInTheDocument();
+    expect(screen.getByText("Forgot Password")).toBeInTheDocument();
+  });
+});
---
+++ src/pages/login/login.tsx
@@ -0,0 +1,5 @@
+const Login = () => {
+  return <p>Sign in</p>;
+};
+
+export default Login;
